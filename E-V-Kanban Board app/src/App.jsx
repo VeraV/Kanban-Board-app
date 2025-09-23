f82267bb942c9b55752cb4f3f65215cc41006bfe
Vera Fileyeva <veremei.vera@gmail.com>
2025-09-23 16:59:45 +0100

    conflict resolve

--- E-V-Kanban Board app/src/App.jsx
+++ E-V-Kanban Board app/src/App.jsx
@@ -1,11 +1,16 @@
 import "./App.css";
 import Sidebar from "./components/Sidebar";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+
 import Logo from "./components/Logo";
 function App() {
   return (
     <div className="Home-page">
+      <NavBar />
       <Sidebar />
       <Logo />
+      <Footer />
     </div>
   );
 }
---
+++ E-V-Kanban Board app/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer>
+      <a href="https://github.com/emidiop-05/Emidio-Vera-Kanban-Board-app">
+        GitHub repository
+      </a>
+    </footer>
+  );
+}
+
+export default Footer;
